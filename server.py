0a250697f459d003039da1b612774325816c4852
gregsadetsky <lepetitg@gmail.com>
2024-11-14 20:15:45 -0500

    .

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>helooooooooooooooooooooooo!</p>"
+    return "<p>ok but surely..............!</p>"
 
 
 app.run(host="0.0.0.0", port=9000)
